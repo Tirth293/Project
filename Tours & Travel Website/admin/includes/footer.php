9923071be3828c5479bbcba156179f19cfe87a67
Tirth293 <tirthparekh293@gmail.com>
2025-09-21 12:02:03 +0530

    Add files via upload

---
+++ Tours & Travel Website/admin/includes/footer.php
@@ -0,0 +1,3 @@
+<div class="copyrights">
+	 <p>©️2024 Tours and Travels Website. All Rights Reserved |  <a href="http://localhost/Tours%20&%20Travel%20%20Website/">Tours and Travels Website</a> </p>
+</div>	
